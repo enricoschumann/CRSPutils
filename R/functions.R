4ab7da01ece12b34977fe4a2fdd9f0bcb5e747f1
Enrico Schumann <es@enricoschumann.net>
2018-11-14 15:32:16 +0100

    [write_daily_files] Overwrite existing files

--- R/functions.R
+++ R/functions.R
@@ -1,27 +1,37 @@
 write_daily_files <- function(filename, outdir, verbose = TRUE) {
     
-    options(scipen = 50)
-    f <- file(filename, "r")
+    old.scipen <- options(scipen = 99)
+    on.exit(options(scipen = old.scipen))
     
-    i <- 0
-    if (!dir.exists(outdir))
+    f <- file(filename, "r")
+    on.exit(close(f))
+    
+    if (!dir.exists(outdir)) {
+        if (verbose)
+            message("create ", normalizePath(outdir))
         dir.create(outdir, recursive = TRUE)
-
-    setwd(outdir)
+    }
+        
+    i <- 0
+    current_permno <- "0"
     repeat {
-        i <- i + 1
         line <- readLines(f, n = 1)
         if (length(line) == 0L)
             break
         
+        i <- i + 1
         spl_line <- strsplit(line, "|", fixed = TRUE)[[1]]
         permno <- spl_line[1L]
+        if (permno != current_permno) {
+            fn <- file.path(outdir, permno)
+            cat("", file = fn)
+            current_permno <- permno
+        }
         cat(spl_line[-1], "\n",
-            file = permno, append = TRUE, sep = ",")
+            file = fn, append = TRUE, sep = ",")
         
         if (verbose && i %% 1000000 == 0)
-            message("a million lines done")
-        
+            message("one million lines processed")        
     }
-    invisible(i - 1)
+    invisible(i)
 }
